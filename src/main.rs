784eb258d0ff7035e9d8f9479ff81063609268b7
Simon Schneemelcher <s.schneemelcher@tu-bs.de>
2021-04-07 17:59:03 +0200

    more precision

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate sdl2; 
 extern crate num_complex;
+extern crate rust_decimal;
 
 
 use sdl2::pixels::Color;
@@ -9,14 +10,16 @@ use sdl2::render::WindowCanvas;
 use sdl2::rect::Point;
 use sdl2::EventPump;
 
+use rust_decimal::prelude::Decimal;
+
 use std::time::Duration;
 use std::process::exit;
  
 pub fn main() {
     let sdl_context     = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-    let window_height   = 200i32;
-    let window_width    = 200i32;
+    let window_height   = 150i32;
+    let window_width    = 150i32;
 
     let window = video_subsystem.window("fractal-explorer", window_width as u32, window_height as u32)
         .position_centered()
@@ -51,7 +54,7 @@ fn find_sdl_gl_driver() -> Option<u32> {
 }
 
 
-fn scale(x: f64, a: f64, b: f64, max: f64) -> f64 {
+fn scale(x: Decimal, a: Decimal, b: Decimal, max: Decimal) -> Decimal {
 
     return ((b-a) * x)/max + a;
 
@@ -64,14 +67,19 @@ fn run(canvas: &mut WindowCanvas, event_pump: &mut EventPump, window_width: i32,
     let mut y_temp        = 0.0f64;
     let mut iteration     = 0i32;
     let mut tmp           = 0f64;
-    let mut x_scaled      = 0f64;
-    let mut y_scaled      = 0f64;
-    let mut x_offset      = 0f64;
-    let mut y_offset      = 0f64;
-    let mut min           = -2f64;
-    let mut max           = 2f64;
+    let mut x_offset      = Decimal::new(0, 0);
+    let mut y_offset      = Decimal::new(0, 0);
+    let mut min           = Decimal::new(-2, 0);
+    let mut max           = Decimal::new(2, 0);
+    let mut x_scaled      = Decimal::new(0, 0);
+    let mut y_scaled      = Decimal::new(0, 0);
+    let width             = Decimal::new(window_width as i64, 0);
+    let height            = Decimal::new(window_height as i64, 0);
+    let move_speed        = Decimal::new(1, 1);
+    let zoom_speed        = Decimal::new(15, 1);
+    let threshold         = Decimal::new(4, 0);
 
-    loop {
+    'running: loop {
         canvas.set_draw_color(Color::RGB(0, 0 , 0));
         canvas.clear();
 
@@ -79,27 +87,28 @@ fn run(canvas: &mut WindowCanvas, event_pump: &mut EventPump, window_width: i32,
             match event {
                 Event::Quit{..}|
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => return 0,
-                Event::KeyDown { keycode: Some(Keycode::Plus), .. } => {min /= 1.5; max /= 1.5;}, 
-                Event::KeyDown { keycode: Some(Keycode::Minus), .. } => {min *= 1.5; max *= 1.5;},
-                Event::KeyDown { keycode: Some(Keycode::Up), .. } => y_offset -= 0.1 * (max-min),
-                Event::KeyDown { keycode: Some(Keycode::Down), .. } => y_offset += 0.1 * (max-min),
-                Event::KeyDown { keycode: Some(Keycode::Left), .. } => x_offset -= 0.1 * (max-min),
-                Event::KeyDown { keycode: Some(Keycode::Right), .. } => x_offset += 0.1 * (max-min),
-                Event::KeyDown { keycode: Some(Keycode::Q), .. } => max_iteration=std::cmp::min(100000000,max_iteration+100),
-                Event::KeyDown { keycode: Some(Keycode::E), .. } => max_iteration=std::cmp::max(0, max_iteration-100),
-                _ => continue
+                Event::KeyDown { keycode: Some(Keycode::Plus), .. }   => {min         /= zoom_speed; max /= zoom_speed;}, 
+                Event::KeyDown { keycode: Some(Keycode::Minus), .. }  => {min         *= zoom_speed; max *= zoom_speed;},
+                Event::KeyDown { keycode: Some(Keycode::Up), .. }     => y_offset     -= move_speed * (max-min),
+                Event::KeyDown { keycode: Some(Keycode::Down), .. }   => y_offset     += move_speed * (max-min),
+                Event::KeyDown { keycode: Some(Keycode::Left), .. }   => x_offset     -= move_speed * (max-min),
+                Event::KeyDown { keycode: Some(Keycode::Right), .. }  => x_offset     += move_speed * (max-min),
+                Event::KeyDown { keycode: Some(Keycode::Q), .. }      => max_iteration = std::cmp::min(100000000,max_iteration+100),
+                Event::KeyDown { keycode: Some(Keycode::E), .. }      => max_iteration = std::cmp::max(0, max_iteration-100),
+                _                                                     => continue 'running
             }
         }
             for x in 0..window_width {
-                x_scaled = scale(x as f64, min + x_offset, max + x_offset, window_width as f64);
+                x_scaled = scale(Decimal::new(x as i64, 0), min + x_offset, max + x_offset, width);
 
                 for y in 0..window_height {
-                    y_scaled = scale(y as f64, min + y_offset, max + y_offset, window_height as f64);
+                    println!("x: {}, y: {}", x, y);
+                    y_scaled = scale(Decimal::new(y as i64, 0), min + y_offset, max + y_offset, height);
                     let c = num_complex::Complex::new(x_scaled, y_scaled);
-                    let mut z = num_complex::Complex::new(0.0, 0.0);    
+                    let mut z = num_complex::Complex::new(Decimal::new(0, 0), Decimal::new(0, 0));    
                     iteration = 0;
 
-                    while z.norm() <= 2f64 && (iteration < max_iteration) {
+                    while ((z.re * z.re) + (z.im * z.im)) <= threshold && (iteration < max_iteration) {
                         z = z.powu(2) + c;
                         iteration += 1;
                     }
