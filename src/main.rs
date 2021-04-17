06cf5a8851543e6a5de233469959edb6bffe8070
Simon Schneemelcher <s.schneemelcher@tu-bs.de>
2021-04-17 17:36:44 +0200

    switched to minifb

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,80 @@
-extern crate sdl2; 
+extern crate minifb;
 extern crate num_complex;
+use minifb::{Key, Window, WindowOptions};
+
+const WIDTH: usize = 500;
+const HEIGHT: usize = 400;
+
+fn scale(x: usize, a: f64, b: f64, max: usize) -> f64 {
+    return ((b-a) * x as f64)/max as f64 + a;}
 
 
-use sdl2::pixels::Color;
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::render::WindowCanvas;
-use sdl2::rect::Point;
-use sdl2::EventPump;
+fn main() {
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
-use std::time::Duration;
-use std::process::exit;
- 
-pub fn main() {
-    let sdl_context     = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-    let window_height   = 200i32;
-    let window_width    = 200i32;
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
 
-    let window = video_subsystem.window("fractal-explorer", window_width as u32, window_height as u32)
-        .position_centered()
-        .opengl()
-        .build()
-        .unwrap();
+    // Limit to max ~60 fps update rate
+//    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
-    let mut canvas = window.into_canvas()
-        .present_vsync()
-        .index(find_sdl_gl_driver().unwrap())
-        .build()
-        .unwrap();
-
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.clear();
-    canvas.present();
-
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    exit(run(&mut canvas, &mut event_pump, window_width, window_height));
-} 
-
-
-fn find_sdl_gl_driver() -> Option<u32> {
-    for (index, item) in sdl2::render::drivers().enumerate() {
-        if item.name == "opengl" {
-            println!("found opengl driver");
-            return Some(index as u32);
-        }
-    }
-    println!("did not find opengl driver");
-    None
-}
-
-
-fn scale(x: f64, a: f64, b: f64, max: f64) -> f64 {
-
-    return ((b-a) * x)/max + a;
-
-} 
-
-fn run(canvas: &mut WindowCanvas, event_pump: &mut EventPump, window_width: i32, window_height: i32) -> i32 {
-    
-    let mut max_iteration = 200;
-    let mut x_temp        = 0.0f64;
-    let mut y_temp        = 0.0f64;
-    let mut iteration     = 0i32;
-    let mut tmp           = 0f64;
-    let mut x_scaled      = 0f64;
-    let mut y_scaled      = 0f64;
+    let mut max_iteration = 200u32;
+    let mut iteration: u32;
+    let mut x_scaled: f64;
+    let mut y_scaled: f64;
     let mut x_offset      = 0f64;
     let mut y_offset      = 0f64;
     let mut min           = -2f64;
-    let mut max           = 2f64;
+    let mut max           = 2f64;       
+    let mut changed: bool = true;
 
-    loop {
-        canvas.set_draw_color(Color::RGB(0, 0 , 0));
-        canvas.clear();
 
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit{..}|
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => return 0,
-                Event::KeyDown { keycode: Some(Keycode::Plus), .. } => {min /= 1.5; max /= 1.5;}, 
-                Event::KeyDown { keycode: Some(Keycode::Minus), .. } => {min *= 1.5; max *= 1.5;},
-                Event::KeyDown { keycode: Some(Keycode::Up), .. } => y_offset -= 0.1 * (max-min),
-                Event::KeyDown { keycode: Some(Keycode::Down), .. } => y_offset += 0.1 * (max-min),
-                Event::KeyDown { keycode: Some(Keycode::Left), .. } => x_offset -= 0.1 * (max-min),
-                Event::KeyDown { keycode: Some(Keycode::Right), .. } => x_offset += 0.1 * (max-min),
-                Event::KeyDown { keycode: Some(Keycode::Q), .. } => max_iteration=std::cmp::min(100000000,max_iteration+100),
-                Event::KeyDown { keycode: Some(Keycode::E), .. } => max_iteration=std::cmp::max(0, max_iteration-100),
-                _ => continue
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        if changed {
+            let mut count: usize = 0;
+            for i in buffer.iter_mut() {
+                x_scaled = scale(count % WIDTH, min + x_offset, max + x_offset, WIDTH);
+                y_scaled = scale(count / HEIGHT, min + y_offset, max + y_offset, HEIGHT);
+                let c = num_complex::Complex::new(x_scaled, y_scaled);
+                let mut z = num_complex::Complex::new(0.0, 0.0);
+                iteration = 0_u32;
+
+                while z.norm() <= 2f64 && (iteration < max_iteration) {
+                    z = z.powu(2) + c;
+                    iteration += 1;
+                }
+
+                *i = iteration.pow(5);
+                count += 1;
             }
         }
-            for x in 0..window_width {
-                x_scaled = scale(x as f64, min + x_offset, max + x_offset, window_width as f64);
-
-                for y in 0..window_height {
-                    y_scaled = scale(y as f64, min + y_offset, max + y_offset, window_height as f64);
-                    let c = num_complex::Complex::new(x_scaled, y_scaled);
-                    let mut z = num_complex::Complex::new(0.0, 0.0);    
-                    iteration = 0;
-
-                    while z.norm() <= 2f64 && (iteration < max_iteration) {
-                        z = z.powu(2) + c;
-                        iteration += 1;
-                    }
-                    iteration %= 255;
-                    canvas.set_draw_color(Color::RGB(iteration as u8, (2 * iteration) as u8, (3 * iteration) as u8));
-                    canvas.draw_point(Point::new(x, y)).ok();
+        changed = false;
+        window.get_keys_pressed(minifb::KeyRepeat::Yes).map(|keys| {
+            for t in keys {
+                changed = true;
+                match t {
+                    Key::Up => y_offset -= 0.1 * (max-min),
+                    Key::Down => y_offset += 0.1 * (max-min),
+                    Key::Left => x_offset -= 0.1 * (max-min),
+                    Key::Right => x_offset += 0.1 * (max-min),
+                    Key::Q => max_iteration=std::cmp::min(100000000,max_iteration+100),
+                    Key::E => max_iteration=std::cmp::max(0, max_iteration-100),
+                    Key::W => {min /= 1.5; max /= 1.5;},
+                    Key::S => {min *= 1.5; max *= 1.5;},
+                    _ => (),
                 }
             }
-            canvas.present();
+        });
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT)
+            .unwrap();
     }
 }
-
